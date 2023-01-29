d5e153b8232d10fe636c0569b03376910fb8e350
elektron1k <fop.holoborshchyi@gmail.com>
2023-01-29 22:09:22 +0200

    Added the section for Analyst’s Comment

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -51,6 +51,21 @@ const Article = () => {
         the global economy’s upturn means believing in cyclicals — regardless of
         what the dollar does.
       </p>
+      <div className="article-expert-comment">
+        <h3 className="article-expert-comment-title">Experts Comment</h3>
+        <p className="article-expert-comment-text">
+          Over long periods of time the market reflects the gradual growth of
+          the larger economy.\nBut many people want to be rich now, or beat the
+          average. This is gambling. It is based on a few stocks for a short
+          period of time, and assumes all the other traders are rational in some
+          way. \nIn the short term the market acts like a herd of cows: always
+          looking to stampede in some direction. In the long run the cows always
+          return to the barn.\nSo, be aware that gambling and a sure bet operate
+          at different time scales. I have no interest in learning enough about
+          any stock, to make day trades. \nAnd investing for the long term has
+          more than doubled my money.
+        </p>
+      </div>
     </div>
   );
 };
